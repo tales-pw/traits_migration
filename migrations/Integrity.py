672c713b1e80e8a61fec21e5a830576dcfd5cf72
Vizmar <49916385+Vizmar@users.noreply.github.com>
2020-09-30 23:23:05 +0500

    Update Integrity.py

--- migrations/Integrity.py
+++ migrations/Integrity.py
@@ -1,22 +1,12 @@
 from database.new_db import DBGameObject, DBTrait
 from database.old_db import DBCharacter
 
-
-def __parse_text_instance(dn: str, db_character: DBCharacter) -> int:
-    try:
-        return int(db_character.get_text(dn, ""))
-    except ValueError:
-        return 0
-
-
 def migrate_integrity(db_character: DBCharacter, db_game_object: DBGameObject) -> None:
-    Integrity = __parse_text_instance("Целостность", db_character)
-
     DBTrait(
-        dn="Опыт",
+        dn="Целостность",
         game_object=db_game_object,
         data={
             "dn": "Целостность",
             "type": "Целостность",
-            "points": Integrity
+            "points": db_character.get_number("Целостность", 7)
         })
